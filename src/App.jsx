278cd1f15b394806ae1b62d455631c687f0f08a7
Ahmed Salem <ahmedsalem292001@gmail.com>
2023-10-12 19:33:32 +0300

    firstCommit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './Components/Header';
+import Lists from './Components/List';
+
+function App() {
+  const [allTasks, setAlltasks] = useState([]);
+
+  function addTask(task) {
+    setAlltasks([...allTasks, task]);
+
+  }
+  return (
+    <div className="AppContainer">
+      <div className="App">
+        <Header onAdd={addTask} />
+        <Lists allTasks={allTasks} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import logo from "../Assets/listLogo.svg";
+import "./Header.css";
+const Header = (props) => {
+    const [task, setTask] = useState("");
+    const submitForm = (e) => {
+        e.preventDefault();
+        props.onAdd(task);
+        setTask("");
+    }
+    return (
+        <div className="headercontainer">
+            <div className="header">
+                <div className="img">
+                    <img src={logo} width="20px" height="25px" alt="logo" />
+                </div>
+                <h3>To-Do-List</h3>
+            </div>
+            <form action="" onSubmit={submitForm}>
+                <input type="text" placeholder="task" className="taskInput"
+                    value={task}
+                    onChange={(e) => { setTask(e.target.value) }} />
+
+                <button type="submit" disabled={!task} className="sunmitbtn">Add</button>
+            </form>
+
+        </div>
+    )
+}
+export default Header;
