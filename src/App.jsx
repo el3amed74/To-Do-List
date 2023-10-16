6f131f89172db07489b52bacb7ecd67f388dc27e
Ahmed Salem <ahmedsalem292001@gmail.com>
2023-10-16 08:11:47 +0300

    make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Lists from './Components/List';
-
+import Palestine from './Components/Palestine';
 function App() {
 
   const [allTasks, setAlltasks] = useState([]);
@@ -10,14 +10,17 @@ function App() {
   function addTask(task) {
     setAlltasks([...allTasks, task]);
   }
-  
+
   return (
-    <div className="AppContainer">
-      <div className="App">
-        <Header onAdd={addTask} />
-        <Lists allTasks={allTasks} />
+    <>
+      <Palestine />
+      <div className="AppContainer">
+        <div className="App">
+          <Header onAdd={addTask} />
+          <Lists allTasks={allTasks} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/Components/Palestine.jsx
@@ -0,0 +1,12 @@
+import "./Palestine.css";
+import PalestineFlage from "../Assets/palestineFlag.png";
+const Palestine = () => {
+    return(
+        <div className="Palestinecontainer">
+            <img src={PalestineFlage} alt="PalestineFlage"  />
+            <h3>Free Palestine</h3>
+            <img src={PalestineFlage} alt="PalestineFlage"  />
+        </div>
+    )
+}
+export default Palestine;
